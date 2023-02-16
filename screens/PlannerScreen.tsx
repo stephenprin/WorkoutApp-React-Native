b996965b8fdca76f72d0f8c01bd68ef74f826a70
stephenprin <stephenprince427@gmail.com>
2023-02-16 09:44:17 +0100

    configuration and setting up the simulator

---
+++ screens/PlannerScreen.tsx
@@ -0,0 +1,9 @@
+import{ View, Text } from 'react-native'
+
+export default function PlannerScreen() { 
+    return (
+        <View>
+            <Text>Planner Screen</Text>
+        </View>
+    )
+}
