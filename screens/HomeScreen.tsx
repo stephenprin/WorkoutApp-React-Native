57b765d44577342fe36d292dfd56119b5ba4f80c
stephenprin <stephenprince427@gmail.com>
2023-02-18 03:48:48 +0100

    home display of data

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,21 +1,49 @@
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, StyleSheet, ImageBackground, FlatList } from 'react-native'
 import { NativeStackHeaderProps } from '@react-navigation/native-stack' 
 import { useEffect } from 'react'
+import data from '../data.json'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 
 type navigationType = NativeStackHeaderProps
 
-export default function HomeScreen({ navigation }: navigationType) { 
-    
-    useEffect(() => { 
-        console.log("HomeScreen mounted")
+export default function HomeScreen({ navigation }: navigationType) {
 
-        return () => console.log('Unmounted home' )
-    }, [])
-    return (
+    const renderItem = ({ item }: any) =>
         <View>
-            <Text>Home Screen</Text>
-            <Button title="Go to Planner" onPress={() => navigation.navigate("Planner")} />
-        </View>
+              <Text style={styles.title}>{item.name}</Text>
+             <Text style={styles.diff}>{item.difficulty}</Text>
+      </View>
+    
+    
+
+    return (
+        <SafeAreaView style={styles.container}>
+        
+            <FlatList
+                data={data}
+                renderItem={ renderItem}
+                keyExtractor={(item) => item.slug}
+            
+            />
+        </SafeAreaView>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+       
+        
+    },
+    title:{
+    fontSize: 16,
+        fontWeight: 'bold',
+        color: 'maroon',
+    },
+    diff: {
+        fontSize: 14,
+        color: 'black',
+    }
+})
